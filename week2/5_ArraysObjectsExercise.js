5d384acbfab239ee80c49a3fed876f8ecca4fdbd
Tara Larsen <talarsen74@gmail.com>
2021-06-29 21:55:05 -0700

    fix parameter in exercise 11 and adjust code for edge cases

--- week2/5_ArraysObjectsExercise.js
+++ week2/5_ArraysObjectsExercise.js
@@ -1,72 +1,72 @@
-// Week 3 - Arrays and Objects
+// // Week 3 - Arrays and Objects
 
-// In the following exercises, you will need to place your code or answer underneath each
-// exercise prompt.
+// // In the following exercises, you will need to place your code or answer underneath each
+// // exercise prompt.
 
-// First try answering these without using references or looking up any information.
-// Then, check your answer by using references and/or running your code.
-// You can run your JS code using the Chrome or Firefox Developer tools, or by using Node.js.
-// Feel free to update your answers if you got them wrong at first -- this exercise is for your own learning.
-// But make sure you understand why the correct answer is right.
+// // First try answering these without using references or looking up any information.
+// // Then, check your answer by using references and/or running your code.
+// // You can run your JS code using the Chrome or Firefox Developer tools, or by using Node.js.
+// // Feel free to update your answers if you got them wrong at first -- this exercise is for your own learning.
+// // But make sure you understand why the correct answer is right.
 
-// Exercise 1. Make an empty array named animals
-let animals = [];
-// Exercise 2. Add the string "frog" to the array
-animals.push("frog"); 
-console.log(animals);
-// Exercise 3. Add 4 more animals of your choice to the array. Try to add all of them in one line of code.
+// // Exercise 1. Make an empty array named animals
+// let animals = [];
+// // Exercise 2. Add the string "frog" to the array
+// animals.push("frog"); 
+// console.log(animals);
+// // Exercise 3. Add 4 more animals of your choice to the array. Try to add all of them in one line of code.
 
-animals.push("deer", "skunk", "raccoon", "bear");
-console.log(animals);
-// Exercise 4. Update the first item in the array to be "gorilla"
+// animals.push("deer", "skunk", "raccoon", "bear");
+// console.log(animals);
+// // Exercise 4. Update the first item in the array to be "gorilla"
 
-animals.splice(0,1, "gorilla");
-console.log(animals);
-// Exercise 5. Print the number of items in the array (use the array property that will give you this information)
+// animals.splice(0,1, "gorilla");
+// console.log(animals);
+// // Exercise 5. Print the number of items in the array (use the array property that will give you this information)
 
-console.log(animals.length);
+// console.log(animals.length);
 
-// Exercise 6. Print the first item in the array
+// // Exercise 6. Print the first item in the array
 
-console.log(animals[0]);
+// console.log(animals[0]);
 
-// Exercise 7. Print the last item in the array (as part of your answer, use the same array property you used in exercise 5 above)
-let lastItem = animals[animals.length -1]
-console.log(lastItem);
+// // Exercise 7. Print the last item in the array (as part of your answer, use the same array property you used in exercise 5 above)
+// let lastItem = animals[animals.length -1]
+// console.log(lastItem);
 
-// Exercise 8. Remove the last item from the array
+// // Exercise 8. Remove the last item from the array
 
-animals.pop()
-console.log(animals);
-// Exercise 9. Now make a new array named assortedThings that contains at least 3 strings and 3 numbers.
+// animals.pop()
+// console.log(animals);
+// // Exercise 9. Now make a new array named assortedThings that contains at least 3 strings and 3 numbers.
 
-let assortedThings = ["paper", 1, "pen", 2, "desk", 3]
-console.log(assortedThings);
-// Exercise 10. Write a for loop that iterates through the assortedThings array and prints each index and item, such as:
-// Item #0 is potato
-// Item #1 is 4
-// Item #2 is Hello World
-// ...
-for(let i = 0; i < assortedThings.length; i++) {
-  console.log(`Item #${i} is ${assortedThings[i]}`);
-}
+// let assortedThings = ["paper", 1, "pen", 2, "desk", 3]
+// console.log(assortedThings);
+// // Exercise 10. Write a for loop that iterates through the assortedThings array and prints each index and item, such as:
+// // Item #0 is potato
+// // Item #1 is 4
+// // Item #2 is Hello World
+// // ...
+// for(let i = 0; i < assortedThings.length; i++) {
+//   console.log(`Item #${i} is ${assortedThings[i]}`);
+// }
 
-// Exercise 11. Write a function that takes an array of numbers as a parameter. For each number in the array,
-// print the number and "BIG" if it's over 100, "small" if it's between 0 and 100, and "negative" if
-// it's less than 0. Example:
-// if the array is [-2, 200, 50], print:
-// -2 negative
-// 200 BIG
-// 50 small
-//
-// Then write some function calls you would use to test your function.
+// // Exercise 11. Write a function that takes an array of numbers as a parameter. For each number in the array,
+// // print the number and "BIG" if it's over 100, "small" if it's between 0 and 100, and "negative" if
+// // it's less than 0. Example:
+// // if the array is [-2, 200, 50], print:
+// // -2 negative
+// // 200 BIG
+// // 50 small
+// //
+// // Then write some function calls you would use to test your function.
+//take extra care on edge cases
 
-
-let printNumbers = ([...arrayOfNumbers]) => {
+let printNumbers = (arrayOfNumbers) => {
   for(let i = 0; i < arrayOfNumbers.length; i++) {
     if(arrayOfNumbers[i] > 100) {
       console.log(`${arrayOfNumbers[i]} BIG`);
-    }else if(arrayOfNumbers[i] > 0 && arrayOfNumbers[i] < 100) {
+    }else if(arrayOfNumbers[i] >= 0) {
       console.log(`${arrayOfNumbers[i]} small`);
     } else {
       console.log(`${arrayOfNumbers[i]} negative`);
@@ -76,38 +76,40 @@ let printNumbers = ([...arrayOfNumbers]) => {
 
 printNumbers([-2, 500, 5]);
 printNumbers([-60, 10000, 2]);
+printNumbers([0, 99.99, 2.0]);
+printNumbers([100, 99.99, 2.0]);
 
 
 
 
 
-// Exercise 12. Make an object called me that contains the following keys: name, favoriteAnimal, favoriteNumber
-// The values of those keys can be whatever you choose.
+// // Exercise 12. Make an object called me that contains the following keys: name, favoriteAnimal, favoriteNumber
+// // The values of those keys can be whatever you choose.
 
-const me = {
-  name: "Tara",
-  favoriteAnimal: "horse",
-  favoriteNumber: "20"
-}
-// Exercise 13. Add another 2 keys and values to the object (you choose what they are)
+// const me = {
+//   name: "Tara",
+//   favoriteAnimal: "horse",
+//   favoriteNumber: "20"
+// }
+// // Exercise 13. Add another 2 keys and values to the object (you choose what they are)
 
-me.favoriteColor = "purple";
-me.favoriteFruit = "blackberry";
+// me.favoriteColor = "purple";
+// me.favoriteFruit = "blackberry";
 
-// Exercise 14. Update the favoriteAnimal value to something different
+// // Exercise 14. Update the favoriteAnimal value to something different
 
-me.favoriteAnimal = "dog"
-console.log(me);
-// Exercise 15. Print the value of favoriteAnimal.
-// Note: there are at least 2 ways to get the value of a key, try to write both.
+// me.favoriteAnimal = "dog"
+// console.log(me);
+// // Exercise 15. Print the value of favoriteAnimal.
+// // Note: there are at least 2 ways to get the value of a key, try to write both.
 
-console.log(me.favoriteAnimal);
-console.log(me["favoriteAnimal"]);
+// console.log(me.favoriteAnimal);
+// console.log(me["favoriteAnimal"]);
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
 // Please answer in a comment below.
-/**** The first part of this exercise was easy. I needed to search for a few things such as splice and printing the last item of the array. I spend the most time on exercise 11. Figuring out how to get the function to take an array of parameters. I used the spread operator.  */
+/**** The first part of this exercise was easy. I needed to search for a few things such as splice and printing the last item of the array. I spend the most time on exercise 11. Figuring out how to get the function to take an array of parameters. I used the spread operator. It turns I didn't need to use the spread operator. */
 
 // Email your file to the course staff,
 // or commit your file to GitHub and email us a link.
